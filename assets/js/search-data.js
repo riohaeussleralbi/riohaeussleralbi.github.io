5a2fe14365ac825c588086a27ac15ef7bf18b283
riohaeussleralbi <rio.haeussleralbi@gmail.com>
2025-12-11 19:15:39 +0000

    Deploying to gh-pages from @ riohaeussleralbi/riohaeussleralbi.github.io@4314203abf265ae83f84c336a7c86ce5dfa6d44b 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -25,7 +25,7 @@ ninja.data = [{
           },
         },{id: "nav-cv",
           title: "cv",
-          description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
+          description: "Below you can find my curriculum vitae. Alternatively, you can download the PDF version by clicking on the icon on the right.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/cv/";
