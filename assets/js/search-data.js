11821a3d5ee98af84578decc7a2ea161bf28ef01
riohaeussleralbi <rio.haeussleralbi@gmail.com>
2025-12-14 05:32:38 +0000

    Deploying to gh-pages from @ riohaeussleralbi/riohaeussleralbi.github.io@03c32b4ccac04679125c5b40e88349796023435d 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -24,7 +24,7 @@ ninja.data = [{
             window.location.href = "/projects/";
           },
         },{id: "nav-teaching",
-          title: "Teaching",
+          title: "teaching",
           description: "Courses I have taught.",
           section: "Navigation",
           handler: () => {
